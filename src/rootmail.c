974ad856bb7bf3d02bf1a01f5e8d27cea766bc3d
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2013-02-09 12:19:35 -0600

    The date in incoming mail can be wonky, so parse it into a canonical form.

--- src/rootmail.c
+++ src/rootmail.c
@@ -43,16 +43,61 @@ rootmail_hook(
 	MsgInfo * const	msginfo = (MsgInfo *) source;
 
 	if( msginfo )	{
+		static char const	delim[] = {
+			"[ \s\t\r\n]"
+		};
 		FolderItem * const	folder = msginfo->folder;
+		gpointer		when;
+		size_t const		Nargv = 32;
+		gpointer		argv[ Nargv ];
+		int			argc;
+		gpointer		s;
 
+		/* Canonicalize the date				 */
+		memset( argv, 0, sizeof( argv ) );
+		when = g_strdup( defstr( msginfo->date ) );
+		for(
+			argc = 0, s = when;
+			(argc < Nargv) && (argv[argc] = strtok( s, delim ));
+			++argc, s = NULL
+		);
+		if( argc < Nargv )	{
+			argv[ argc ] = NULL;
+		}
+		/*
+		 *-------------------------------------------------------------
+		 * We expect the tokens to be:
+		 * argv[0] = Day abbrev
+		 * argv[1] = Day of month
+		 * argv[2] = Month abbrev
+		 * argv[3] = Year XXXX
+		 * argv[4] = %H:%M:%S
+		 * argv[5] = Timezone offset
+		 *-------------------------------------------------------------
+		 */
+		/* Show our work					 */
 		fprintf(
 			NewLog,
-			"%-23.23s  %-31.31s  %-54.54s  %.80s\n",
+			"%-23.23s  "
+			"%4.4s "
+			"%2.2s "
+			"%3.3s "
+			"%4.4s "
+			"%8.8s "
+			"%5.5s  "
+			"%-54.54s  "
+			"%.80s\n",
 			folder ? defstr( folder->name ) : "",
-			defstr( msginfo->date ),
+			defstr( argv[ 0 ] ),
+			defstr( argv[ 1 ] ),
+			defstr( argv[ 2 ] ),
+			defstr( argv[ 3 ] ),
+			defstr( argv[ 4 ] ),
+			defstr( argv[ 5 ] ),
 			defstr( msginfo->from ),
 			defstr( msginfo->subject )
 		);
+		g_free( when );
 	}
 	return( FALSE );
 }
